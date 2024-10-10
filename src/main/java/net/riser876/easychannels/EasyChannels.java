644ac59718a6c89612985116fe503b55f197b2d8
GMalvestiti <gustavo.smalvestiti@gmail.com>
2024-10-09 21:39:59 -0300

    [feat] Initial commit
    Signed-off-by: GMalvestiti <gustavo.smalvestiti@gmail.com>

---
+++ src/main/java/net/riser876/easychannels/EasyChannels.java
@@ -0,0 +1,16 @@
+package net.riser876.easychannels;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EasyChannels implements ModInitializer {
+
+    public static final String MOD_ID = "easychannels";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+    }
+}
